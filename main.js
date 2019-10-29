f66f603e217ccc3890be88315d4a7cae978a59ae
emil1003 <columbus-pc@hotmail.com>
2019-10-29 08:38:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+/*	GulagBot v0.0.1
+	(c) 2019 Henning Emil Bøjer Bach
+*/
+
+//Require Discord.js
+const Discord = require('discord.js');
+
+//Require settings.json
+const settings = require("./settings.json");
+
+const client = new Discord.Client();
+
+let CEGuild = null;
+
+let radioChannel = null;
+let radioStream = null;
+let dispatcher = null;
+
+client.on("ready", () => {
+	console.log("Event: ready");
+
+	console.log(`I am ${client.user.tag}`);
+	
+	CEGuild = client.guilds.get("575052713239511080");
+	
+	if (CEGuild.available) {
+		console.log(`${CEGuild.name} (${CEGuild.id}) connected`);
+	} else {
+		return;
+	}
+})
+
+client.on("message", message => {
+	if (!message.author.bot) {
+
+		if (message.guild == null || message.guild.id != CEGuild.id) {
+			//I am somewhere where I shouldn't be
+			message.channel.send("where am i? (I should be in Communist Enthusiasts)");
+			console.log(`WARN: Bot outside of Communist Enthusiasts!
+	${message.author.tag} contacted bot in DMs`);
+			return;
+		}
+		
+		switch (message.content.toLowerCase().split(" ")[0]) {
+			case "!radio":
+				console.log("Radio triggered by " + message.author.username);
+				radioChannel = CEGuild.channels.find(val => val.name === "Soviet Radio");
+				radioChannel.join()
+					.then(connection => {
+						radioStream = connection;
+						console.log("Joined radio channel");
+						dispatcher = connection.playFile("/home/henning/Musik/National Anthem of the soviet union - Red Army Choir-BulFwGSi8bc.mp3");
+						
+					});
+				message.delete();
+				break;
+			case "!quit":
+				//Stop playing
+				console.log(`Quit triggered by ${message.author.username}`);
+				console.log("Leaving radio channel");
+				(dispatcher != null) && dispatcher.end();
+				radioChannel.leave();
+				message.delete();
+				break;
+			case "!gulag":
+				if (message.mentions.everyone) {
+					message.channel.send("YOU CANNOT SEND THEM TO THE GULAG!");
+					break;
+				}
+				
+				var toGulag = message.mentions.members.first();
+				if (toGulag.user.id == message.author.id) {
+					message.channel.send(`HEY EVERYONE! ${message.author.username} IS AN IDIOT!`);
+				} else {
+					toGulag.addRole("638310306182987779")
+							.catch(console.error);
+					message.channel.send(`${toGulag.user.username} IS GOING TO THE GULAG!`);
+				}
+				break;
+			case "!pardon":
+				if (message.mentions.everyone) {
+					message.channel.send("YOU CANNOT PARDON THEM!");
+					break;
+				}
+
+				var toPardon = message.mentions.members.first();
+				if (toPardon.user.id == message.author.id) {
+					message.channel.send("NICE TRY CAPITALIST PIG!");
+				} else {
+					toPardon.removeRole("638310306182987779")
+						.catch(console.error);
+					message.channel.send(`${toPardon.user.username} IS A FRIEND OF THE SOVIET UNION AGAIN!`);
+				}
+				break;
+		}
+	}
+})
+
+console.log("GulagBot v0.0.1");
+
+client.login(settings.token);
+
